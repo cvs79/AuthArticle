2df8e3d05919239b5331ca37ecc395f725edd4dd
Chris <cvs79@msn.com>
2018-10-17 08:10:54 +0200

    Security Updates

--- Auth0WebArticle/Auth0WebArticle/Controllers/AccountController.cs
+++ Auth0WebArticle/Auth0WebArticle/Controllers/AccountController.cs
@@ -3,28 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Http.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace Auth0WebArticle.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Login()
+        public async Task Login(string returnUrl = "/")
         {
-            return new ChallengeResult("Auth0", new AuthenticationProperties() { RedirectUri = "/" });
+            await HttpContext.ChallengeAsync("Auth0", new AuthenticationProperties() { RedirectUri = returnUrl });
         }
 
         [Authorize]
         public async Task Logout()
         {
-            await HttpContext.Authentication.SignOutAsync("Auth0", new AuthenticationProperties
+            await HttpContext.SignOutAsync("Auth0", new AuthenticationProperties
             {
+                // Indicate here where Auth0 should redirect the user after a logout.
+                // Note that the resulting absolute Uri must be whitelisted in the
+                // **Allowed Logout URLs** settings for the app.
                 RedirectUri = Url.Action("Index", "Home")
             });
-            await HttpContext.Authentication.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-        }
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        }       
 
         [Authorize]
         public ActionResult Claims()
--- Auth0WebArticle/Auth0WebArticle/Startup.cs
+++ Auth0WebArticle/Auth0WebArticle/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -32,125 +33,84 @@ namespace Auth0WebArticle
         public void ConfigureServices(IServiceCollection services)
         {
             // Add authentication services
-            services.AddAuthentication(
-                options => options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme);
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            })
+            .AddCookie()
+            .AddOpenIdConnect("Auth0", options =>
+            {
+                // Set the authority to your Auth0 domain
+                options.Authority = $"https://{Configuration["Auth0:Domain"]}";
+
+                // Configure the Auth0 Client ID and Client Secret
+                options.ClientId = Configuration["Auth0:ClientId"];
+                options.ClientSecret = Configuration["Auth0:ClientSecret"];
+
+                // Set response type to code
+                options.ResponseType = "code";
+
+                // Configure the scope
+                options.Scope.Clear();
+                options.Scope.Add("openid");
+
+                // Set the callback path, so Auth0 will call back to http://localhost:5000/signin-auth0
+                // Also ensure that you have added the URL as an Allowed Callback URL in your Auth0 dashboard
+                options.CallbackPath = new PathString("/signin-auth0");
+
+                // Configure the Claims Issuer to be Auth0
+                options.ClaimsIssuer = "Auth0";
+
+                options.Events = new OpenIdConnectEvents
+                {
+                    // handle the logout redirection
+                    OnRedirectToIdentityProviderForSignOut = (context) =>
+                            {
+                                var logoutUri = $"https://{Configuration["Auth0:Domain"]}/v2/logout?client_id={Configuration["Auth0:ClientId"]}";
+
+                                var postLogoutUri = context.Properties.RedirectUri;
+                                if (!string.IsNullOrEmpty(postLogoutUri))
+                                {
+                                    if (postLogoutUri.StartsWith("/"))
+                                    {
+                                // transform to absolute
+                                var request = context.Request;
+                                        postLogoutUri = request.Scheme + "://" + request.Host + request.PathBase + postLogoutUri;
+                                    }
+                                    logoutUri += $"&returnTo={ Uri.EscapeDataString(postLogoutUri)}";
+                                }
+
+                                context.Response.Redirect(logoutUri);
+                                context.HandleResponse();
+
+                                return Task.CompletedTask;
+                            }
+                };
+            });
 
             // Add framework services.
-            services.AddMvc();
-
-            // Add functionality to inject IOptions<T>
-            services.AddOptions();
-
-            // Add the Auth0 Settings object so it can be injected
-            services.Configure<Auth0Settings>(Configuration.GetSection("Auth0"));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IOptions<Auth0Settings> auth0Settings)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseBrowserLink();
             }
             else
             {
                 app.UseExceptionHandler("/Home/Error");
             }
 
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseCookiePolicy();
 
-            // Add the cookie middleware
-            app.UseCookieAuthentication(new CookieAuthenticationOptions
-            {
-                AutomaticAuthenticate = true,
-                AutomaticChallenge = true
-            });
-
-            // Add the OIDC middleware
-            var options = new OpenIdConnectOptions("Auth0")
-            {
-                // Set the authority to your Auth0 domain
-                Authority = $"https://{auth0Settings.Value.Domain}",
-
-                // Configure the Auth0 Client ID and Client Secret
-                ClientId = auth0Settings.Value.ClientId,
-                ClientSecret = auth0Settings.Value.ClientSecret,
-
-                // Do not automatically authenticate and challenge
-                AutomaticAuthenticate = false,
-                AutomaticChallenge = false,
-
-                // Set response type to code
-                ResponseType = "code",
-
-                // Set the callback path, so Auth0 will call back to http://localhost:5000/signin-auth0 
-                // Also ensure that you have added the URL as an Allowed Callback URL in your Auth0 dashboard 
-                CallbackPath = new PathString("/signin-auth0"),
-
-                // Configure the Claims Issuer to be Auth0
-                ClaimsIssuer = "Auth0",
-
-                // Saves tokens to the AuthenticationProperties
-                SaveTokens = true,
-
-                Events = new OpenIdConnectEvents()
-                {
-                    OnTicketReceived = context =>
-                    {
-                        // Get the ClaimsIdentity
-                        var identity = context.Principal.Identity as ClaimsIdentity;
-                        if (identity != null)
-                        {
-                            // Check if token names are stored in Properties
-                            if (context.Properties.Items.ContainsKey(".TokenNames"))
-                            {
-                                // Token names a semicolon separated
-                                string[] tokenNames = context.Properties.Items[".TokenNames"].Split(';');
-
-                                // Add each token value as Claim
-                                foreach (var tokenName in tokenNames)
-                                {
-                                    // Tokens are stored in a Dictionary with the Key ".Token.<token name>"
-                                    string tokenValue = context.Properties.Items[$".Token.{tokenName}"];
-
-                                    identity.AddClaim(new Claim(tokenName, tokenValue));
-                                }
-                            }
-                        }
-
-                        return Task.FromResult(0);
-                    },
-                    // handle the logout redirection 
-                    OnRedirectToIdentityProviderForSignOut = (context) =>
-                    {
-                        var logoutUri = $"https://{auth0Settings.Value.Domain}/v2/logout?client_id={auth0Settings.Value.ClientId}";
-
-                        var postLogoutUri = context.Properties.RedirectUri;
-                        if (!string.IsNullOrEmpty(postLogoutUri))
-                        {
-                            if (postLogoutUri.StartsWith("/"))
-                            {
-                                // transform to absolute
-                                var request = context.Request;
-                                postLogoutUri = request.Scheme + "://" + request.Host + request.PathBase + postLogoutUri;
-                            }
-                            logoutUri += $"&returnTo={ Uri.EscapeDataString(postLogoutUri)}";
-                        }
-
-                        context.Response.Redirect(logoutUri);
-                        context.HandleResponse();
-
-                        return Task.CompletedTask;
-                    }
-                }
-            };
-            options.Scope.Clear();
-            options.Scope.Add("openid");
-            app.UseOpenIdConnectAuthentication(options);
+            app.UseAuthentication();
 
             app.UseMvc(routes =>
             {
